eba180c9f7cf84ea36bbf49cd3cdea08e01d341a
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-03-05 20:44:24 +0530

    Create CurrentReading.c

---
+++ CurrentReading.c
@@ -0,0 +1,15 @@
+#include "CurrentReading.h"
+
+
+int NumberofContinuousRange(int *ChargingCurrentReadings, int numOfCCReadings)
+{
+    int i,count = 0;
+    for(i=0;i<numOfCCReadings;i++)
+    {
+        if(((ChargingCurrentReadings[i+1] -  ChargingCurrentReadings[i]) == 0) && (ChargingCurrentReadings[i+1] -  ChargingCurrentReadings[i]) == 1))
+        {
+            count++;
+        }
+    }
+    return count;
+}
