4c021453f740d7c0a58f576d58262cba9825fe21
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-03-05 22:52:31 +0530

    Update test_CurrentReading.cpp

--- test_CurrentReading.cpp
+++ test_CurrentReading.cpp
@@ -4,7 +4,7 @@
 #include "CurrentReading.h"
 
 TEST_CASE("Case for detecting number of continuous range in the given reading:Failing") {
-  int ChargingCurrentReadings[] = {3, 3, 5, 4, 10, 11, 12};
+  int ChargingCurrentReadings[] = {3, 3, 4, 5};
   int numOfCCReadings = sizeof(ChargingCurrentReadings) / sizeof(ChargingCurrentReadings[0]);
-  REQUIRE(NumberofContinuousRange(ChargingCurrentReadings, numOfCCReadings) == 0);
+  REQUIRE(NumberofContinuousRange(ChargingCurrentReadings, numOfCCReadings) == 4);
 }
