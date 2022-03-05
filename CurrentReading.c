a0655372f0f6da6be82d57cff39caf819b544b03
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-03-05 23:05:23 +0530

    Update CurrentReading.c

--- CurrentReading.c
+++ CurrentReading.c
@@ -7,7 +7,7 @@ int NumberofContinuousRange(int *ChargingCurrentReadings, int numOfCCReadings)
     for(i=0;i<numOfCCReadings;i++)
     {
         curRead = (ChargingCurrentReadings[i+1] -  ChargingCurrentReadings[i]);
-        if((curRead == 0) && (curRead == 1))
+        if((curRead == 0) || (curRead == 1))
         {
             count++;
         }
