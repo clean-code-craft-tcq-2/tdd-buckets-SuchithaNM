b92e7fac9ebb9bdbc9c07153a3dba38d98f64f66
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-03-05 20:36:18 +0530

    Update test_CurrentReading.cpp

--- test_CurrentReading.cpp
+++ test_CurrentReading.cpp
@@ -1,4 +1,5 @@
 #include "catch.hpp"
+#include "CurrentRead.h"
 
 TEST_CASE("Case for detecting number of continuous range in the given reading:Failing") {
   int ChargingCurrentReadings[] = {3, 3, 5, 4, 10, 11, 12};
