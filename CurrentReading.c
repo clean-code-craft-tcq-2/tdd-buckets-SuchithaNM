17b31eb0452522c92bb1b2402d5516c3842d8974
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-03-13 22:23:58 +0530

    Update CurrentReading.c

--- CurrentReading.c
+++ CurrentReading.c
@@ -1,11 +1,44 @@
 #include "CurrentReading.h"
-int NumberofContRange=0;
+int NumberofContRange = 0;
 
 int count = 1;
+
+bool validateInputSequence(int *InputSequence , int length)
+{
+	int i;
+	for(i= 0; i < length; i++)
+	{
+		if(InputSequence[i] >= 0)
+		{
+			sortInputSeq(InputSequence, length);			
+			return TRUE;
+		}	
+		else
+			return FALSE;
+	}
+
+}
+void sortInputSeq(int *InputSequence , int length)
+{
+	int i , j , temp ;
+	for(i=0; i< length; i++)
+	{
+		for(j=i+1; j< length; j++)
+		{
+			if(InputSequence[i] > InputSequence[j])
+			{
+				temp = InputSequence[i];
+				InputSequence[i] = InputSequence[j];
+				InputSequence[j] = temp; 
+			}
+		}	
+	}
+}
 
 int NumberofContinuousRange(int *ChargingCurrentReadings, int numOfCCReadings)
 {
     int i,curRead;
+    if(validateInputSequence(ChargingCurrentReadings, numOfCCReadings) == TRUE)
     for(i=0;i<numOfCCReadings;i++)
     {
         curRead = (ChargingCurrentReadings[i+1] -  ChargingCurrentReadings[i]);
