074b5601d3ad26a8cd1900efd4749878de171d3a
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-03-13 22:48:31 +0530

    Update test_CurrentReading.cpp

--- test_CurrentReading.cpp
+++ test_CurrentReading.cpp
@@ -3,21 +3,25 @@
 #include "test/catch.hpp"
 #include "CurrentReading.h"
 
+TEST_CASE("Case for detecting number of continuous range in the given reading:Failing") {
+  int ChargingCurrentReadings[] = {4,5};
+  int numOfCCReadings = sizeof(ChargingCurrentReadings) / sizeof(ChargingCurrentReadings[0]);
+  REQUIRE(validateInputSequence(ChargingCurrentReadings,numOfCCReadings) == true);
+  REQUIRE(NumberofContinuousRange(ChargingCurrentReadings, numOfCCReadings) == 2);
+}
+
 TEST_CASE("Case for checking the given array is invalid with Negative numbers") {
   int ChargingCurrentReadings[] = {3, 3 ,5, -1, -3, 11, 12};
   int numOfCCReadings = sizeof(ChargingCurrentReadings) / sizeof(ChargingCurrentReadings[0]);
-  REQUIRE(NumberofContinuousRange(ChargingCurrentReadings, numOfCCReadings) == 2);
+  REQUIRE(validateInputSequence(ChargingCurrentReadings,numOfCCReadings) == false);
 }
 
 TEST_CASE("Case for checking the given array is continuous and arraged in the ascending oder") {
   int ChargingCurrentReadings[] = {3, 3 ,5, 4, 11, 10, 12};
   int numOfCCReadings = sizeof(ChargingCurrentReadings) / sizeof(ChargingCurrentReadings[0]);
+  REQUIRE(validateInputSequence(ChargingCurrentReadings,numOfCCReadings) == true);
   REQUIRE(NumberofContinuousRange(ChargingCurrentReadings, numOfCCReadings) == 2);
 }
 
-TEST_CASE("Case for detecting number of continuous range in the given reading:Failing") {
-  int ChargingCurrentReadings[] = {4,5};
-  int numOfCCReadings = sizeof(ChargingCurrentReadings) / sizeof(ChargingCurrentReadings[0]);
-  REQUIRE(NumberofContinuousRange(ChargingCurrentReadings, numOfCCReadings) == 2);
-}
+
 
