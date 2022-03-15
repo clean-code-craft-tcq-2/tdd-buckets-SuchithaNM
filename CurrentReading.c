fb9cf8c25925b8795a7eb13eed8d45044c3ca80b
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-03-15 20:57:55 +0530

    Update CurrentReading.c

--- CurrentReading.c
+++ CurrentReading.c
@@ -3,7 +3,7 @@ int NumberofContRange = 0;
 
 int count = 0;
 
-bool validateInputSequence(int *InputSequence , int length)
+bool validateCurrentReadingInputSequence(int *InputSequence , int length)
 {
 	int i;
 	for(i= 0; i < length; i++)
@@ -40,22 +40,22 @@ void sortInputSeq(int *InputSequence , int length)
 int NumberofContinuousRange(int *ChargingCurrentReadings, int numOfCCReadings)
 {
     int i, j, curRead;
-    if(validateInputSequence(ChargingCurrentReadings, numOfCCReadings) == true)
+    if(validateCurrentReadingInputSequence(ChargingCurrentReadings, numOfCCReadings) == true)
     {
 	    for(i = 0; i < numOfCCReadings; i++)
 	    {
 		curRead = (ChargingCurrentReadings[i+1] -  ChargingCurrentReadings[i]);
-		if(curRead > 1)
+		if((curRead == 0) || (curRead == 1))
 		{
-			break;
+			count++;
 		}
-		count++;
+		
 	    }
 	   return count;
     }
 }
 
-RangeReading printFunction( int min, int max, int numberInRange)
+RangeReading detectCurrentReadingAndPrintCSVFormat( int min, int max, int numberInRange)
 {
 	RangeReading rangeReading;
 	rangeReading.StartIndex=min;
