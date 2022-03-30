69c679bbd382e8b55dca38da5821a1b4ac0960d4
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-03-30 18:30:09 +0530

    Update CurrentReading.h

--- CurrentReading.h
+++ CurrentReading.h
@@ -13,6 +13,6 @@ typedef struct{
 extern int numberofContinuousRange( int *ChargingCurrentReadings, int numOfCCReadings);
 extern bool validateCurrentReadingInputSequence(int *InputSequence , int length);
 extern void sortInputSeq(int *InputSequence , int length);
-RangeReading detectCurrentReadingAndFormatOutputInCSVFormat( int min, int max, int numberInRange, char * ChargingCurrentReadings)
+RangeReading detectCurrentReadingAndFormatOutputInCSVFormat( int min, int max, int numberInRange, char * ChargingCurrentReadings);
 void printOutputInCSV(char * ChargingCurrentReadings, int numberInRange);
 
